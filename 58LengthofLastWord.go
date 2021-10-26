189ffcc2de99656f26e23def88a5a469dafcbe53
Kevin Diu <kevindiujp@gmail.com>
2021-10-27 00:46:49 +0900

    Update 58LengthofLastWord.go

--- 58LengthofLastWord.go
+++ 58LengthofLastWord.go
@@ -1,12 +1,15 @@
 // https://leetcode.com/problems/length-of-last-word/
 func lengthOfLastWord(s string) int {
     lolw := 0
-    for i := len(s)-1; i >= 0; i-- {
+    i := len(s)-1
+    for ; i >= 0; i-- {
+        if s[i] != ' ' {
+            break
+        }
+    }
+    for ; i>= 0; i-- {
         if s[i] == ' ' {
-            if lolw == 0 {
-                continue
-            }
-            return lolw
+            break
         }
         lolw++
     }
