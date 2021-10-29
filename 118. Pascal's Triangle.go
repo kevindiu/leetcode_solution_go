67fb519ffe7cce1971086699a8e5e596233691da
Kevin Diu <kevindiujp@gmail.com>
2021-10-29 21:55:20 +0900

    Create 118. Pascal's Triangle.go

---
+++ 118. Pascal's Triangle.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/pascals-triangle/
+func generate(numRows int) [][]int {
+    res := make([][]int, numRows)
+    for i := 0; i < numRows; i++ {
+        res[i] = make([]int, i + 1)
+        res[i][0] = 1
+        res[i][i] = 1
+    }
+    
+    for i := 2; i < numRows; i++ {
+        for j := 1; j < len(res[i]) -1; j++ {
+            res[i][j] = res[i-1][j-1] + res[i-1][j]
+        }
+    }
+    
+    return res
+}
